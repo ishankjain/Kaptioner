2bae554f81bf8cd85e4aae4e72c7092089c76a27
ishankjain <ishank.jain67@gmail.com>
2019-03-09 00:21:26 +0530

    build the vocabulary

---
+++ build_vocab.py
@@ -0,0 +1,119 @@
+import nltk
+import pickle
+import argparse
+from collections import Counter
+from pycocotools.coco import COCO
+import collections
+import argparse
+import torch
+import torch.nn as nn
+import numpy as np
+import os
+import pickle
+import torch.utils.data as data
+from torch.nn.utils.rnn import pack_padded_sequence
+from torchvision import transforms
+from PIL import Image
+import nltk
+import torchvision.transforms as transforms
+import collections
+from pycocotools.coco import COCO
+import time
+import json
+
+class meraCOCO:
+    def __init__(self, annotation_file=None):
+        """
+        Constructor of Microsoft COCO helper class for reading and visualizing annotations.
+        :param annotation_file (str): location of annotation file
+        :param image_folder (str): location to the folder that hosts images.
+        :return:
+        """
+        # load dataset
+        self.dataset,self.anns = dict(),dict()
+        self.imgToAnns = collections.defaultdict(list)
+        if not annotation_file == None:
+            print('loading annotations into memory...')
+            tic = time.time()
+            dataset = json.load(open(annotation_file, 'r'))
+            assert type(dataset)==dict, 'annotation file format {} not supported'.format(type(dataset))
+            print('Done (t={:0.2f}s)'.format(time.time()- tic))
+            self.dataset = dataset
+            self.meracreateIndex()
+
+    def meracreateIndex(self):
+        # create index
+        print('creating index...')
+        anns = {}
+        imgToAnns = collections.defaultdict(list)
+        id=1
+        if 'annotations' in self.dataset:
+            for ann in self.dataset['annotations']:
+                imgToAnns[ann['image_id']].append(ann)
+                anns[id] = ann
+                id+=1
+        print('index created!')
+
+        # create class members
+        self.anns = anns
+        self.imgToAnns = imgToAnns
+        
+
+class Vocabulary(object):
+    """Simple vocabulary wrapper."""
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = {}
+        self.idx = 0
+
+    def add_word(self, word):
+        if not word in self.word2idx:
+            self.word2idx[word] = self.idx
+            self.idx2word[self.idx] = word
+            self.idx += 1
+
+    def __call__(self, word):
+        if not word in self.word2idx:
+            return self.word2idx['<unk>']
+        return self.word2idx[word]
+
+    def __len__(self):
+        return len(self.word2idx)
+
+def build_vocab(json, threshold):
+    """Build a simple vocabulary wrapper."""
+    coco = meraCOCO(json)
+    counter = Counter()
+    ids = coco.anns.keys()
+    for i, id in enumerate(ids):
+        caption = str(coco.anns[id]['caption'])
+        tokens = nltk.tokenize.word_tokenize(caption.lower())
+        counter.update(tokens)
+
+        if (i+1) % 1000 == 0:
+            print("[{}/{}] Tokenized the captions.".format(i+1, len(ids)))
+
+    # If the word frequency is less than 'threshold', then the word is discarded.
+    words = [word for word, cnt in counter.items() if cnt >= threshold]
+
+    # Create a vocab wrapper and add some special tokens.
+    vocab = Vocabulary()
+    vocab.add_word('<pad>')
+    vocab.add_word('<start>')
+    vocab.add_word('<end>')
+    vocab.add_word('<unk>')
+
+    # Add the words to the vocabulary.
+    for i, word in enumerate(words):
+        vocab.add_word(word)
+    return vocab
+
+# def main(args):
+vocab = build_vocab(json='data/ibm/train_captions.json', threshold=2)
+vocab_path = 'data/ibm/vocab.pkl'
+with open(vocab_path, 'wb') as f:
+    pickle.dump(vocab, f)
+print("Total vocabulary size: {}".format(len(vocab)))
+print("Saved the vocabulary wrapper to '{}'".format(vocab_path))
+
+
