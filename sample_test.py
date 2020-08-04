fe620a0b2210d6f1c8cb9c6103333aed64772ad6
ishankjain <31738717+ishankjain@users.noreply.github.com>
2020-08-04 16:06:47 +0530

    comments added

--- sample_test.py
+++ sample_test.py
@@ -1,76 +1,73 @@
-import torch
-import matplotlib.pyplot as plt
-import numpy as np 
-import argparse
-import pickle 
-import os
-from torchvision import transforms 
-from build_vocab import Vocabulary
-from model import EncoderCNN, DecoderRNN
+import numpy as np
 from PIL import Image
+from pickle import load
+import matplotlib.pyplot as plt
+from keras.models import load_model
+from keras.preprocessing.image import load_img, img_to_array
+from utils.model import CNNModel, generate_caption_beam_search
+import os
+
+from config import config
+
+"""
+    *Some simple checking
+"""
+assert type(config['max_length']) is int, 'Please provide an integer value for `max_length` parameter in config.py file'
+assert type(config['beam_search_k']) is int, 'Please provide an integer value for `beam_search_k` parameter in config.py file'
+
+# Extract features from each image in the directory
+def extract_features(filename, model, model_type):
+	if model_type == 'inceptionv3':
+		from keras.applications.inception_v3 import preprocess_input
+		target_size = (299, 299)
+	elif model_type == 'vgg16':
+		from keras.applications.vgg16 import preprocess_input
+		target_size = (224, 224)
+	# Loading and resizing image
+	image = load_img(filename, target_size=target_size)
+	# Convert the image pixels to a numpy array
+	image = img_to_array(image)
+	# Reshape data for the model
+	image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+	# Prepare the image for the CNN Model model
+	image = preprocess_input(image)
+	# Pass image into model to get encoded features
+	features = model.predict(image, verbose=0)
+	return features
+
+# Load the tokenizer
+tokenizer_path = config['tokenizer_path']
+tokenizer = load(open(tokenizer_path, 'rb'))
+
+# Max sequence length (from training)
+max_length = config['max_length']
+
+# Load the model
+caption_model = load_model(config['model_load_path'])
+
+image_model = CNNModel(config['model_type'])
 
 
-
-def load_image(image_path, transform=None):
-    image = Image.open(image_path)
-    image = image.resize([224, 224], Image.LANCZOS)
-    
-    if transform is not None:
-        image = transform(image).unsqueeze(0)
-    
-    return image
-
-def main(args):
-    transform = transforms.Compose([
-        transforms.ToTensor(), 
-        transforms.Normalize((0.485, 0.456, 0.406), 
-                             (0.229, 0.224, 0.225))])
-    # Load vocabulary wrapper
-    with open(vocab_path, 'rb') as f:
-        vocab = pickle.load(f)
-    # Build models
-    encoder = EncoderCNN(embed_size).eval()  # eval mode (batchnorm uses moving mean/variance)
-    decoder = DecoderRNN(embed_size, hidden_size, len(vocab), num_layers)
-    #     encoder = encoder.to(device)
-    #     decoder = decoder.to(device)
-    
-    # Load the trained model parameters
-    encoder.load_state_dict(torch.load(encoder_path))
-    decoder.load_state_dict(torch.load(decoder_path))
-    
-    # Prepare an image
-    image = load_image(image, transform)
-    image_tensor = image#.to(device)
-        
-    # Generate an caption from the image
-    feature = encoder(image_tensor)
-    sampled_ids = decoder.sample(feature)
-    sampled_ids = sampled_ids[0].cpu().numpy()          # (1, max_seq_length) -> (max_seq_length)
-        
-    # Convert word_ids to words
-    sampled_caption = []
-    for word_id in sampled_ids:
-        word = vocab.idx2word[word_id]
-        sampled_caption.append(word)
-        if word == '<end>':
-            break
-    sentence = ' '.join(sampled_caption)
-        
-    # Print out the image and the generated caption
-    print (sentence)
-
-    
-
- if __name__ == '__main__':
-     parser = argparse.ArgumentParser()
-     parser.add_argument('--image', type=str, required=True, help='input image for generating caption')
-     parser.add_argument('--encoder_path', type=str, default='models/encoder-5-3000.ckpt', help='path for trained encoder')
-     parser.add_argument('--decoder_path', type=str, default='models/decoder-5-3000.ckpt', help='path for trained decoder')
-     parser.add_argument('--vocab_path', type=str, default='data/vocab.pkl', help='path for vocabulary wrapper')
-   
-     # Model parameters (should be same as paramters in train.py)
-     parser.add_argument('--embed_size', type=int , default=256, help='dimension of word embedding vectors')
-     parser.add_argument('--hidden_size', type=int , default=512, help='dimension of lstm hidden states')
-     parser.add_argument('--num_layers', type=int , default=1, help='number of layers in lstm')
-     args = parser.parse_args()
-     main(args)
+# Load and prepare the image
+for image_file in os.listdir(config['test_data_path']):
+	if(image_file.split('--')[0]=='output'):
+		continue
+	if(image_file.split('.')[1]=='jpg' or image_file.split('.')[1]=='jpeg'):
+		print('Generating caption for {}'.format(image_file))
+		# Encode image using CNN Model
+		image = extract_features(config['test_data_path']+image_file, image_model, config['model_type'])
+		# Generate caption using Decoder RNN Model + BEAM search
+		generated_caption = generate_caption_beam_search(caption_model, tokenizer, image, max_length, beam_index=config['beam_search_k'])
+		# Remove startseq and endseq
+		caption = 'Caption: ' + generated_caption.split()[1].capitalize()
+		for x in generated_caption.split()[2:len(generated_caption.split())-1]:
+		    caption = caption + ' ' + x
+		caption += '.'
+		# Show image and its caption
+		pil_im = Image.open(config['test_data_path']+image_file, 'r')
+		fig, ax = plt.subplots(figsize=(8, 8))
+		ax.get_xaxis().set_visible(False)
+		ax.get_yaxis().set_visible(False)
+		_ = ax.imshow(np.asarray(pil_im), interpolation='nearest')
+		_ = ax.set_title("BEAM Search with k={}\n{}".format(config['beam_search_k'],caption),fontdict={'fontsize': '20','fontweight' : '40'})
+		plt.savefig(config['test_data_path']+'output--'+image_file)
