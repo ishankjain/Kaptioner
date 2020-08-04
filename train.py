42410c3b4741caaacdc43067a4418af79c5f3878
ishankjain <31738717+ishankjain@users.noreply.github.com>
2020-08-04 16:04:16 +0530

    Update train.py
    add comments

--- train.py
+++ train.py
@@ -1,281 +1,96 @@
-import torch
-import torch.nn as nn
-import numpy as np
-import os
-import pickle
-import torch.utils.data as data
-# from data_loader import get_loader 
-# from model import EncoderCNN, DecoderRNN
-from torch.nn.utils.rnn import pack_padded_sequence
-from torchvision import transforms
-from PIL import Image
-import nltk
-import collections
-import time
-import json
+from pickle import load
+from utils.model import *
+from utils.load_data import loadTrainData, loadValData, data_generator, loadtestData
+from tensorflow.keras.callbacks import ModelCheckpoint
+from config import config, rnnConfig
+import random
+# Setting random seed for reproducibility of results
+random.seed(config['random_seed'])
 
-model_path='data/ibm/models'
-vocab_path='data/ibm/vocab.pkl'
-image_dir='data/ibm/IITD_Hackathon_Dataset/images/'
-# caption_path='data/ibm/IITD_Hackathon_Dataset/Dataset/Task2/train_captions.json'
-caption_path='data/ibm/train_captions.json'
-batch_size=32
-num_workers=2
-embed_size=256
-hidden_size=512
-num_layers=1
-learning_rate=0.001
-num_epochs=5
-crop_size=224
-log_step=10
-save_step=1000
+"""
+    *Some simple checking
+"""
+assert type(config['num_of_epochs']) is int, 'Please provide an integer value for `num_of_epochs` parameter in config.py file'
+assert type(config['max_length']) is int, 'Please provide an integer value for `max_length` parameter in config.py file'
+assert type(config['batch_size']) is int, 'Please provide an integer value for `batch_size` parameter in config.py file'
+assert type(config['beam_search_k']) is int, 'Please provide an integer value for `beam_search_k` parameter in config.py file'
+assert type(config['random_seed']) is int, 'Please provide an integer value for `random_seed` parameter in config.py file'
+assert type(rnnConfig['embedding_size']) is int, 'Please provide an integer value for `embedding_size` parameter in config.py file'
+assert type(rnnConfig['LSTM_units']) is int, 'Please provide an integer value for `LSTM_units` parameter in config.py file'
+assert type(rnnConfig['dense_units']) is int, 'Please provide an integer value for `dense_units` parameter in config.py file'
+assert type(rnnConfig['dropout']) is float, 'Please provide a float value for `dropout` parameter in config.py file'
 
-    # Create model directory
-if not os.path.exists(model_path):
-    os.makedirs(model_path)
-# Image preprocessing, normalization for the pretrained resnet
-transform = transforms.Compose([ 
-    transforms.RandomCrop(crop_size),
-    transforms.RandomHorizontalFlip(), 
-    transforms.ToTensor(), 
-    transforms.Normalize((0.485, 0.456, 0.406),                              
-                         (0.229, 0.224, 0.225))])
-    
-# Load vocabulary wrapper
-with open(vocab_path, 'rb') as f:
-    vocab = pickle.load(f)
-#     print(f.)
+"""
+	*Load Data
+	*X1 : Image features
+	*X2 : Text features(Captions)
+"""
+X1train, X2train, max_length = loadTrainData(config)
 
-class meraCOCO:
-    def __init__(self, annotation_file=None):
-        """
-        Constructor of Microsoft COCO helper class for reading and visualizing annotations.
-        :param annotation_file (str): location of annotation file
-        :param image_folder (str): location to the folder that hosts images.
-        :return:
-        """
-        # load dataset
-        self.dataset,self.anns = dict(),dict()
-        self.imgToAnns = collections.defaultdict(list)
-        if not annotation_file == None:
-            print('loading annotations into memory...')
-            tic = time.time()
-            dataset = json.load(open(annotation_file, 'r'))
-            assert type(dataset)==dict, 'annotation file format {} not supported'.format(type(dataset))
-            print('Done (t={:0.2f}s)'.format(time.time()- tic))
-            self.dataset = dataset
-            self.meracreateIndex()
+X1val, X2val = loadValData(config)
 
-    def meracreateIndex(self):
-        # create index
-        print('creating index...')
-        anns = {}
-        imgToAnns = collections.defaultdict(list)
-        id=1
-        if 'annotations' in self.dataset:
-            for ann in self.dataset['annotations']:
-                imgToAnns[ann['image_id']].append(ann)
-                anns[id] = ann
-                id+=1
-        print('index created!')
+"""
+	*Load the tokenizer
+"""
+tokenizer = load(open(config['tokenizer_path'], 'rb'))
+vocab_size = len(tokenizer.word_index) + 1
 
-        # create class members
-        self.anns = anns
-        self.imgToAnns = imgToAnns
-        
-class cocodataset(data.Dataset):
-    """COCO Custom Dataset compatible with torch.utils.data.DataLoader."""
-    def __init__(self, root, json, vocab, transform=None):
-        """Set the path for images, captions and vocabulary wrapper.
-        
-        Args:
-            root: image directory.
-            json: coco annotation file path.
-            vocab: vocabulary wrapper.
-            transform: image transformer.
-        """
-        self.root = root
-        self.coco = meraCOCO(json)
-        self.ids = list(self.coco.anns.keys())
-        self.vocab = vocab
-        self.transform = transform
-    
-    def __getitem__(self, index):
-        """Returns one data pair (image and caption)."""
-        coco = self.coco
-        vocab = self.vocab
-        ann_id = self.ids[index]
-        caption = coco.anns[ann_id]['caption']
-        img_id = coco.anns[ann_id]['image_id']
-        path = img_id #coco.loadImgs(img_id)[0]['file_name']
+"""
+	*Now that we have the image features from CNN model, we need to feed them to a RNN Model.
+	*Define the RNN model
+"""
+# model = RNNModel(vocab_size, max_length, rnnConfig, config['model_type'])
+model = AlternativeRNNModel(vocab_size, max_length, rnnConfig, config['model_type'])
+print('RNN Model (Decoder) Summary : ')
+print(model.summary())
 
-        image = Image.open(os.path.join(self.root, path)).convert('RGB')
-        if self.transform is not None:
-            image = self.transform(image)
+"""
+    *Train the model save after each epoch
+"""
+num_of_epochs = config['num_of_epochs']
+batch_size = config['batch_size']
+steps_train = len(X2train)//batch_size
+if len(X2train)%batch_size!=0:
+    steps_train = steps_train+1
+steps_val = len(X2val)//batch_size
+if len(X2val)%batch_size!=0:
+    steps_val = steps_val+1
+model_save_path = config['model_data_path']+"model_"+str(config['model_type'])+"_epoch-{epoch:02d}_train_loss-{loss:.4f}_val_loss-{val_loss:.4f}.hdf5"
+checkpoint = ModelCheckpoint(model_save_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+callbacks = [checkpoint]
 
-        # Convert caption (string) to word ids.
-        tokens = nltk.tokenize.word_tokenize(str(caption).lower())
-        caption = []
-        caption.append(vocab('<start>'))
-        caption.extend([vocab(token) for token in tokens])
-        caption.append(vocab('<end>'))
-        target = torch.Tensor(caption)
-        return image, target
+print('steps_train: {}, steps_val: {}'.format(steps_train,steps_val))
+print('Batch Size: {}'.format(batch_size))
+print('Total Number of Epochs = {}'.format(num_of_epochs))
 
-    def __len__(self):
-        return len(self.ids)
+# Shuffle train data
+ids_train = list(X2train.keys())
+random.shuffle(ids_train)
+X2train_shuffled = {_id: X2train[_id] for _id in ids_train}
+X2train = X2train_shuffled
 
+# Create the train data generator
+# returns [[img_features, text_features], out_word]
+generator_train = data_generator(X1train, X2train, tokenizer, max_length, batch_size, config['random_seed'])
+# Create the validation data generator
+# returns [[img_features, text_features], out_word]
+generator_val = data_generator(X1val, X2val, tokenizer, max_length, batch_size, config['random_seed'])
 
-def collate_fn(data):
-    """Creates mini-batch tensors from the list of tuples (image, caption).
-    
-    We should build custom collate_fn rather than using default collate_fn, 
-    because merging caption (including padding) is not supported in default.
+# Fit for one epoch
+model.fit_generator(generator_train,
+            epochs=num_of_epochs,
+            steps_per_epoch=steps_train,
+            validation_data=generator_val,
+            validation_steps=steps_val,
+            callbacks=callbacks,
+            verbose=1)
 
-    Args:
-        data: list of tuple (image, caption). 
-            - image: torch tensor of shape (3, 256, 256).
-            - caption: torch tensor of shape (?); variable length.
+"""
+	*Evaluate the model on validation data and ouput BLEU score
+"""
+print('Model trained successfully. Running model on validation set for calculating BLEU score using BEAM search with k={}'.format(config['beam_search_k']))
+evaluate_model_beam_search(model, X1val, X2val, tokenizer, max_length, beam_index=config['beam_search_k'])
 
-    Returns:
-        images: torch tensor of shape (batch_size, 3, 256, 256).
-        targets: torch tensor of shape (batch_size, padded_length).
-        lengths: list; valid length for each padded caption.
-    """
-    # Sort a data list by caption length (descending order).
-    data.sort(key=lambda x: len(x[1]), reverse=True)
-    images, captions = zip(*data)
-
-    # Merge images (from tuple of 3D tensor to 4D tensor).
-    images = torch.stack(images, 0)
-
-    # Merge captions (from tuple of 1D tensor to 2D tensor).
-    lengths = [len(cap) for cap in captions]
-    targets = torch.zeros(len(captions), max(lengths)).long()
-    for i, cap in enumerate(captions):
-        end = lengths[i]
-        targets[i, :end] = cap[:end]        
-    return images, targets, lengths
-# Build data loader
-# data_loader = get_loader(image_dir, caption_path, vocab, 
-#                          transform, batch_size,
-#                          shuffle=True, num_workers=num_workers) 
-coco = cocodataset(root=image_dir,
-                       json=caption_path,
-                       vocab=vocab,
-                       transform=transform)
-
-data_loader = torch.utils.data.DataLoader(dataset=coco, 
-                                              batch_size=batch_size,
-                                              shuffle=True,
-                                              num_workers=num_workers,
-                                              collate_fn=collate_fn)
-
-import torch
-import torch.nn as nn
-import torchvision.models as models
-from torch.nn.utils.rnn import pack_padded_sequence
-from torch.autograd import Variable
-import numpy as np
-class EncoderCNN(nn.Module):
-    def __init__(self, embed_size):
-        """Load the pretrained ResNet-152 and replace top fc layer."""
-        super(EncoderCNN, self).__init__()
-        resnet = models.resnet152(pretrained=True)
-        modules = list(resnet.children())[:-1]      # delete the last fc layer.
-        self.resnet = nn.Sequential(*modules)
-        self.linear = nn.Linear(resnet.fc.in_features, embed_size)
-        self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
-        self.init_weights()
-    def init_weights(self):
-        """Initialize the weights."""
-        self.linear.weight.data.normal_(0.0, 0.02)
-        self.linear.bias.data.fill_(0)
-
-    def forward(self, images):
-        """Extract the image feature vectors."""
-        features = self.resnet(Variable(images))
-        features = features.data
-        features = features.view(features.size(0), -1)
-        features = self.linear(features)#self.bn(self.linear(features))
-        return features
-
-
-class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers, max_seq_length=20):
-        """Set the hyper-parameters and build the layers."""
-        super(DecoderRNN, self).__init__()
-        self.embed = nn.Embedding(vocab_size, embed_size)
-        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
-        self.linear = nn.Linear(hidden_size, vocab_size)
-        self.max_seg_length = max_seq_length
-        self.init_weights()
-        
-    def init_weights(self):
-        """Initialize weights."""
-        self.embed.weight.data.uniform_(-0.1, 0.1)
-        self.linear.weight.data.uniform_(-0.1, 0.1)
-        self.linear.bias.data.fill_(0)
-        
-    def forward(self, features, captions, lengths):
-        """Decode image feature vectors and generates captions."""
-        embeddings = self.embed(captions)
-        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
-        packed = pack_padded_sequence(embeddings, lengths, batch_first=True) 
-        hiddens, _ = self.lstm(packed)
-        outputs = self.linear(hiddens[0])
-        return outputs
-    
-    def sample(self, features, states=None):
-        """Generate captions for given image features using greedy search."""
-        sampled_ids = []
-        inputs = features.unsqueeze(1)
-        for i in range(self.max_seg_length):
-            hiddens, states = self.lstm(inputs, states)          # hiddens: (batch_size, 1, hidden_size)
-            outputs = self.linear(hiddens.squeeze(1))            # outputs:  (batch_size, vocab_size)
-            _, predicted = outputs.max(1)                        # predicted: (batch_size)
-            sampled_ids.append(predicted)
-            inputs = self.embed(predicted)                       # inputs: (batch_size, embed_size)
-            inputs = inputs.unsqueeze(1)                         # inputs: (batch_size, 1, embed_size)
-        sampled_ids = torch.stack(sampled_ids, 1)                # sampled_ids: (batch_size, max_seq_length)
-        return sampled_ids
-    
-# Build the models
-encoder = EncoderCNN(embed_size)
-decoder = DecoderRNN(embed_size, hidden_size, len(vocab), num_layers)
-
-# Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-params = list(decoder.parameters()) + list(encoder.linear.parameters()) + list(encoder.bn.parameters())
-optimizer = torch.optim.Adam(params, lr=learning_rate)    
-
-nltk.download('punkt')
-
-# Train the models
-total_step = len(data_loader)
-for epoch in range(num_epochs):
-    for i, (images, captions, lengths) in enumerate(data_loader):        
-        # Set mini-batch dataset
-#         images = images.to(device)
-#         captions = captions.to(device)
-        targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]  
-        # Forward, backward and optimize
-        print(i)
-#         print(images)
-        features = encoder(images)
-        outputs = decoder(features, captions, lengths)
-        loss = criterion(outputs, targets)
-        decoder.zero_grad()
-        encoder.zero_grad()
-        loss.backward()
-        optimizer.step()
-        # Print log info
-        if i % log_step == 0:
-            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Perplexity: {:5.4f}'
-                  .format(epoch, num_epochs, i, total_step, loss.item(), np.exp(loss.item()))) 
-        # Save the model checkpoints
-        if i == 93:
-            torch.save(decoder.state_dict(), os.path.join(
-                model_path, 'decoder-{}-{}.ckpt'.format(epoch+1, i+1)))
-            torch.save(encoder.state_dict(), os.path.join(
-                model_path, 'encoder-{}-{}.ckpt'.format(epoch+1, i+1)))
+X1test, X2test = loadtestData(config)
+print('Model trained successfully. Running model on validation set for calculating BLEU score using BEAM search with k={}'.format(config['beam_search_k']))
+evaluate_model_beam_search(model, X1test, X2test, tokenizer, max_length, beam_index=config['beam_search_k'])
